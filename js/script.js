4ea28e138661074484bea2a1e19956a3c627e5dd
Guilherme <silvagui.lacerda.com>
2025-06-19 12:59:09 -0300

    Design da Pokedex terminado

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const fetchPokemon = async (pokemon) => {
 
-    const await APIResponse = fetch('https://pokeapi.co/api/v2/pokemon/${pokemon}'
-    );
+    const  APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}` );
+    const Data = await APIResponse.json();
+    console.log(APIResponse)
 }
+fetchPokemon(25)
