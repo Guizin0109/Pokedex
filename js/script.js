4596473be1df0d74403b5a29de05f097342da0a7
Aluno.Etec <aula@etec.com>
2025-06-23 16:14:21 -0300

    Começou JS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
+const pokemonName = document.querySelector('.pokemon__name');
+
+const pokemonNumber = document.querySelector('.pokemon__number');
+
+
 const fetchPokemon = async (pokemon) => {
 
     const  APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}` );
     const Data = await APIResponse.json();
-    console.log(APIResponse)
+    
+    return Data;
 }
-fetchPokemon(25)
+
+const renderpokemon = async (pokemon) => {
+
+    const data = await fetchPokemon(pokemon);
+
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+
+    console.log(data)
+    
+    
+}
+    renderpokemon('3')
