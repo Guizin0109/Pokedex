c01f3ebf588b6b8bee4637b608641fbd5d7646ef
Guilherme <silvagui.lacerda.com>
2025-06-25 11:11:34 -0300

    Falta colocar os tipos

--- js/script.js
+++ js/script.js
@@ -7,12 +7,20 @@ const form = document.querySelector('.form');
 
 const input = document.querySelector('.input__search');
 
+const buttonprev = document.querySelector('.btn-prev');
+
+
+const buttonnext = document.querySelector('.btn-next');
+
+let searchPokemon = 1 ;
+
+
 
 
 
 const fetchPokemon = async (pokemon) => {
     
-    const  APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}` );
+    const  APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}` );
 
     if(APIResponse.status == 200) {
     const Data = await APIResponse.json();
@@ -24,14 +32,18 @@ const renderpokemon = async (pokemon) => {
 
         pokemonName.innerHTML = 'Loading...';
         pokemonNumber.innerHTML = '';
+
             const data = await fetchPokemon(pokemon);
     if(data) {
+       
+    pokemonImage.style.display = 'block';
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
     pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']; 
-
+        searchPokemon= data.id
     input.value = '';
     } else {
+        pokemonImage.style.display = 'none';
         pokemonName.innerHTML = 'Not Found!';
     }
     
@@ -39,8 +51,24 @@ const renderpokemon = async (pokemon) => {
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     
-    renderpokemon(input.value)
+    renderpokemon(input.value.toLowerCase())
+});
+buttonprev.addEventListener('click', () => {
+   if (searchPokemon>1){
+    searchPokemon -= 1;
+    renderpokemon(searchPokemon);
+   }
+   else {
+
+   }
+    
+});
+buttonnext.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderpokemon(searchPokemon);
+    
 });
 
-renderpokemon('1')
+
+renderpokemon(searchPokemon)
     
